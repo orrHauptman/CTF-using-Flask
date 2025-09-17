0708632e0d2829e2e3a0533024d981a45e79d065
Orr Hauptman <or.hau2008@gmail.com>
2025-09-17 05:26:18 +0300

    stage 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask , request , render_template
 import base64
-
+from datetime import datetime
+import socket
 clues: dict = {
     "/ Clue" : base64.b64encode("Well done! But top programers don't need clues!!!!".encode()).decode(),
     "title clue" : base64.b64encode("I love robots (;".encode()).decode()
@@ -30,8 +31,29 @@ def index_html():
 
 #stage 1 
 @app.route("/robots.txt")
-def robots():
-    return render_template("robots.txt" , )
+def stage1():
+    return render_template("robots.txt" )
+
+#stage 2
+
+@app.route("/night_shift")
+def stage2():
+    now = datetime.now()
+
+    if now.hour == 5:
+        
+        send_UDP_packet("Go to /number .number is the last number printed on the screen")
+
+        return render_template("night_shift.html")
+    
+    return "The night shift starts at 5:00 and ends in 6:00 "
+
+
+def send_UDP_packet(message : str):
+    client = socket.socket(socket.AF_INET ,socket.SOCK_DGRAM)
+    client.connect(("127.0.0.1" ,9912 ))
+    client.send(message.encode())
+
 
 
 if __name__ == "__main__":
